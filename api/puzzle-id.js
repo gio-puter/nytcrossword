8035a5706c8cff07f8dccf897b176ee1d6296927
gio-puter <gxc2001@gmail.com>
2024-08-08 01:52:01 -0700

    Update puzzle-id.js

--- api/puzzle-id.js
+++ api/puzzle-id.js
@@ -6,7 +6,7 @@ export default async function getRecentPuzzleID(req, res) {
         const puzzleResp = await axios.get("https://www.nytimes.com/svc/crosswords/v3/puzzles.json", {
             headers: {Cookie: `NYT-S=${process.env.NYT_COOKIE}`},
         });
-        console.log(puzzleResp);
+        
         const puzzleInfo = puzzleResp.data.results[0];
         const puzzleId = puzzleInfo.puzzle_id;
         res.status(200).json({ puzzleId: puzzleId});
