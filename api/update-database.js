398aff0f9ddea5260b31a9de252473a720cd8fb4
gio-puter <gxc2001@gmail.com>
2024-12-07 14:18:21 -0800

    Update update-database.js

--- api/update-database.js
+++ api/update-database.js
@@ -4,55 +4,40 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 
 export default async function updateDatabase(request, response) {
     try {
-        const id = await getRecentPuzzleID();
+        const puzzleId = await getRecentPuzzleID();
 
-        const puz = await fetch(`https://www.nytimes.com/svc/crosswords/v2/puzzle/${id}.json`, {
+        const puzzle = await fetch(`https://www.nytimes.com/svc/crosswords/v2/puzzle/${puzzleId}.json`, {
             headers: { 'Cookie': `NYT-S=${process.env.NYT_COOKIE}` }
         });
 
-        const puzResp = await puz.json()
-        return response.status(200).json({message: puzResp})
-    } catch (error) {
-        return response.status(500).json({ message: 'Failed to update the database', error: error.message})
-    }
-
-    // try {
-    //     const puzzleId = await getRecentPuzzleID();
-
-    //     const puzzle = await fetch(`https://www.nytimes.com/svc/crosswords/v2/puzzle/${puzzleId}.json`, {
-    //         headers: { 'Cookie': `NYT-S=${process.env.NYT_COOKIE}` }
-    //     });
-
-    //     const puzzleResp = await puzzle.json()
+        const puzzleResp = await puzzle.json()
         
-    //     const puzzleData = puzzleResp.results[0];
+        const puzzleData = puzzleResp.results[0];
 
-    //     const puzzleWidth = puzzleData.puzzle_meta.width;
-    //     const puzzleDotw = puzzleData.puzzle_meta.printDotw;
-    //     const puzzleDate = puzzleData.print_date;
-    //     const acrossClues = puzzleData.puzzle_data.clues.A;
-    //     const downClues = puzzleData.puzzle_data.clues.D;
-    //     const puzzleFill = puzzleData.puzzle_data.answers;
+        const puzzleWidth = puzzleData.puzzle_meta.width;
+        const puzzleDotw = puzzleData.puzzle_meta.printDotw;
+        const puzzleDate = puzzleData.print_date;
+        const acrossClues = puzzleData.puzzle_data.clues.A;
+        const downClues = puzzleData.puzzle_data.clues.D;
+        const puzzleFill = puzzleData.puzzle_data.answers;
 
-    //     const acrossSet = setFromClues(acrossClues, puzzleDate, puzzleDotw, puzzleFill)
-    //     const downSet = setFromClues(downClues, puzzleDate, puzzleDotw, puzzleFill, puzzleWidth)
+        const acrossSet = setFromClues(acrossClues, puzzleDate, puzzleDotw, puzzleFill)
+        const downSet = setFromClues(downClues, puzzleDate, puzzleDotw, puzzleFill, puzzleWidth)
 
-    //     const answerSet = [...acrossSet, ...downSet]
+        const answerSet = [...acrossSet, ...downSet]
 
-    //     const {data, error} = await supabase
-    //         .from("daily_answers")
-    //         .upsert(answerSet, {onConflict: ['clue', 'answer'], ignoreDuplicates: true});
+        const {data, error} = await supabase
+            .from("daily_answers")
+            .upsert(answerSet, {onConflict: ['clue', 'answer'], ignoreDuplicates: true});
 
-    //     if (error) {
-    //         return response.status(500).json({ message: 'Failed to update the database', data: data, error: error.message})
-    //     }
+        if (error) {
+            return response.status(500).json({ message: 'Failed to update the database', data: data, error: error.message})
+        }
 
-    //     return response.status(200).json({ message: 'Database successfully updated', pushedSet: answerSet});
-    // } catch (error) {
-    //     return response.status(500).json({ message: 'Failed to update the database', error: error.message, response: puzResp})
-    // }
-
-
+        return response.status(200).json({ message: 'Database successfully updated', pushedSet: answerSet});
+    } catch (error) {
+        return response.status(500).json({ message: 'Failed to update the database', error: error.message, response: puzResp})
+    }
 }
 
 function setFromClues(clueSet, puzzleDate, puzzleDotw, puzzleFill, step = 1) {
